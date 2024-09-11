763f0a9261f60c4578e758e9ece7754df62f8eff
andre-ortner <andre-ortner@users.noreply.github.com>
2024-09-11 10:46:52 +0000

    Changed design to white

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from settings import settings_page  # Import settings_page from settings.py
 # Set up the Streamlit page configuration
 st.set_page_config(page_title="Dashboard", layout="wide")
 
+# Add a logo at the top of the sidebar
+st.sidebar.image("dks_logo.png", use_column_width=True)
+
 # Sidebar Navigation using option_menu
 with st.sidebar:
     selected = option_menu(
@@ -13,12 +16,20 @@ with st.sidebar:
         icons=["people", "gear"],
         menu_icon="cast", default_index=0,
         styles={
-            "container": {"padding": "5!important", "background-color": "#262730"},
-            "icon": {"color": "white", "font-size": "20px"},
-            "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px", "color": "white"},
-            "nav-link:hover": {"background-color": "#444444"},
-            "nav-link-selected": {"background-color": "#FF0000", "color": "white"},
+            "container": {"padding": "5!important", "background-color": "#ffffff"},  # White background
+            "icon": {"color": "#000000", "font-size": "20px"},  # Black icons
+            "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px", "color": "#000000"},  # Black text
+            "nav-link-selected": {"background-color": "#FF0000", "color": "white"},  # Highlighted selection
+            "menu-title": {"color": "black", "font-size": "20px"},  # White menu title
         }
+        # black style
+        #styles={
+        #    "container": {"padding": "5!important", "background-color": "#262730"},
+        #    "icon": {"color": "white", "font-size": "20px"},
+        #    "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px", "color": "white"},
+        #    "nav-link:hover": {"background-color": "#444444"},
+        #    "nav-link-selected": {"background-color": "#FF0000", "color": "white"},
+        #}
     )
 
 # Dynamically load the selected page by calling its function
